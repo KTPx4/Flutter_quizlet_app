d30e241a88b2d8a6524ab0879e9ca71f2068ba7c
Julio <tuananhnoice03@gmail.com>
2024-05-07 20:36:23 +0700

    test

--- client_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-
 import flutter_tts
 import path_provider_foundation
 import shared_preferences_foundation
@@ -14,5 +13,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-
 }
