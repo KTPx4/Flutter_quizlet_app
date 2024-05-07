5d0f2c387465fd6a0bf82defd334092f9a575db4
phuc mguyen <phucnguyenhoang3839@gmail.com>
2024-05-07 10:00:13 +0700

    version 3

--- server/index.js
+++ server/index.js
@@ -9,20 +9,25 @@ const HomeRouter = require('./routes/HomeRouter')
 const AccountRouter = require('./routes/AccountRouter')
 
 
+
+
+
+
+
 // Define variable 
 const _APP = express()
 const _PORT = process.env.PORT || 3000
 
 // Config 
 _APP.use(express.json())
-_APP.use(express.urlencoded({extended: true}))
+_APP.use(express.urlencoded({ extended: true }))
 
 _APP.use(cors())
 
-_APP.use((req, res, next)=>{
-    req.vars = {root: __dirname}
+_APP.use((req, res, next) => {
+    req.vars = { root: __dirname }
     next()
- })
+})
 
 
 
@@ -31,35 +36,35 @@ _APP.use('/api/', HomeRouter)
 
 _APP.use('/api/account', AccountRouter)
 
-_APP.use('*', (req, res)=>{
+_APP.use('*', (req, res) => {
     res.status(404).json({
         status: "Not Found",
         message: "The url not support"
     })
 })
 // Define Function
- 
-const startProgram = async()=>{
+
+const startProgram = async () => {
     // console.log(await bcrypt.hash("12345", 10));
-    
-    
-     await require('./models/DB')
-     .then(async()=>{
 
-         console.log("Connect DB Success");   
 
-        _APP.listen(_PORT, () => {
-            console.log("App listen at: http://localhost:" + _PORT);
+    await require('./models/DB')
+        .then(async () => {
+
+            console.log("Connect DB Success");
+
+            _APP.listen(_PORT, () => {
+                console.log("App listen at: http://localhost:" + _PORT);
+            })
+
         })
-    
-     })    
-     .catch((err)=>{
-         console.log("Connect DB Failed: ", err);
-     })
-   
- }
- 
- // Run Function 
- startProgram()
+        .catch((err) => {
+            console.log("Connect DB Failed: ", err);
+        })
+
+}
+
+// Run Function 
+startProgram()
 
 
